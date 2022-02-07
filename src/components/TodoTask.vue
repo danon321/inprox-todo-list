65073d8d8e5a2be880470f8ae4556197a166376c
danon321 <danieldobroczek@wp.pl>
2022-02-07 16:02:06 +0100

    Fixed designe on mobile. Use grid to place task-icon. Upgrade 'save' and 'remove' icons look

--- src/components/TodoTask.vue
+++ src/components/TodoTask.vue
@@ -1,28 +1,33 @@
 <template>
-    <div :class="{before_icon : isPhotoBeforeIcon}">
+    <div>
         <div class="task card" v-if="!editMode" @click="editMode = true">
             <div class="task__number">{{index + 1}} </div>
-            <div class="card__title task__title">{{task.title}}</div>
-            <div class="card__desc task__desc">{{task.description}} </div>
+            <div class="task__content">
+                <div class="card__title task__title">{{task.title}}</div>
+                <div class="card__desc task__desc">{{task.description}} </div>
+            </div>
         </div>
         <div class="task card" v-else>
             <div class="task__exit" @click="editMode = false">X</div> 
-            <input class="card__input task__title" type="text" v-model="title"/>
-            <textarea class="card__textarea task__desc" type="text" v-model="description" />
-            <div class="task__buttons">
-                <button class="button task__buttons--save" @click="editTask(index); editMode = false;">
-                    <span class="button__text ">Save</span>
-                </button>
-                <button class="button task__buttons--remove" 
-                @click="$emit('removeTask', index), editMode = false;">
-                    <span class="button__text">Remove</span>
-                </button>
+            <div class="task__content">
+                <input class="card__input task__title" type="text" v-model="title"/>
+                <textarea class="card__textarea task__desc" type="text" v-model="description" />
+                <div class="task__buttons">
+                    <button class="button task__buttons--remove" 
+                    @click="$emit('removeTask', index), editMode = false;">
+                        <span class="button__text">Remove</span>
+                    </button>
+                    <button class="button task__buttons--save" @click="editTask(index); editMode = false;">
+                        <img :src="saveIcon" width="20" alt="Ok - icon">
+                    </button>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import saveIcon from '@/assets/icons/save-icon.svg';
 
 export default {
     name: 'TodoTask',
@@ -34,7 +39,8 @@ export default {
         return{
             title: this.task.title,
             description: this.task.description,
-            editMode: false
+            editMode: false,
+            saveIcon
         }
     },
     methods: {
@@ -44,19 +50,10 @@ export default {
                 description: this.description
             }, index);
         }
-    },
-    computed: {
-        isPhotoBeforeIcon(){
-            if(this.index < 2) return true; else return false;
-        }
     }
 }
 </script>
 
 <style lang="scss">
     @import "@/styles/layout/_task.scss";
-
-    .before_icon{
-        order: -1;
-    }
 </style>
